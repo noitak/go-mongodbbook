9067d65440597f019f2c2c8ea46d6716fc3f0a36
noitak <noitak@me.com>
2016-11-17 16:48:16 +0900

    implement ch01

---
+++ README.md
@@ -0,0 +1,8 @@
+# go-mongobook
+
+[MongoDBの薄い本](http://www.cuspy.org/diary/2012-04-17/the-little-mongodb-book-ja.pdf)の内容をGoで試してみる.
+
+## Run
+	$ mkdir data
+	$ mongod --config mongodb.config
+	$ go run mongodbbook.go
---
+++ mongodbbook.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"time"
+)
+
+type Unicorn struct {
+	ID       bson.ObjectId `bson:"_id,omitempty"`
+	Name     string
+	Dob      time.Time
+	Loves    []string
+	Weight   int
+	Gender   string
+	Vampires int
+}
+
+func insertUnicorns(s *mgo.Session) (*mgo.Collection, error) {
+	c := s.DB("test").C("unicorn")
+	err := c.Insert(
+		&Unicorn{Name: "Horny", Dob: time.Date(1992, 2, 13, 7, 47, 0, 0, time.Local), Loves: []string{"carrot", "papaya"}, Weight: 600, Gender: "m", Vampires: 63},
+		&Unicorn{Name: "Aurora", Dob: time.Date(1991, 0, 24, 13, 0, 0, 0, time.Local), Loves: []string{"carrot", "grape"}, Weight: 450, Gender: "f", Vampires: 43},
+		&Unicorn{Name: "Unicrom", Dob: time.Date(1973, 1, 9, 22, 10, 0, 0, time.Local), Loves: []string{"energon", "redbull"}, Weight: 984, Gender: "m", Vampires: 182},
+		&Unicorn{Name: "Roooooodles", Dob: time.Date(1979, 7, 18, 18, 40, 0, 0, time.Local), Loves: []string{"apple"}, Weight: 575, Gender: "m", Vampires: 99},
+		&Unicorn{Name: "Solnara", Dob: time.Date(1985, 6, 4, 2, 10, 0, 0, time.Local), Loves: []string{"apple", "carrot", "chocolate"}, Weight: 550, Gender: "f", Vampires: 80},
+		&Unicorn{Name: "Ayna", Dob: time.Date(1998, 2, 7, 8, 30, 0, 0, time.Local), Loves: []string{"strawberry", "lemon"}, Weight: 733, Gender: "f", Vampires: 40},
+		&Unicorn{Name: "Kenny", Dob: time.Date(1997, 6, 1, 10, 420, 0, 0, time.Local), Loves: []string{"grape", "lemon"}, Weight: 690, Gender: "m", Vampires: 39},
+		&Unicorn{Name: "Raleigh", Dob: time.Date(205, 4, 3, 0, 570, 0, 0, time.Local), Loves: []string{"apple", "sugar"}, Weight: 421, Gender: "m", Vampires: 2},
+		&Unicorn{Name: "Leia", Dob: time.Date(201, 9, 8, 14, 530, 0, 0, time.Local), Loves: []string{"apple", "watermelon"}, Weight: 601, Gender: "f", Vampires: 33},
+		&Unicorn{Name: "Pilot", Dob: time.Date(1997, 2, 1, 5, 30, 0, 0, time.Local), Loves: []string{"apple", "watermelon"}, Weight: 650, Gender: "m", Vampires: 54},
+		&Unicorn{Name: "Nimue", Dob: time.Date(1999, 11, 20, 16, 150, 0, 0, time.Local), Loves: []string{"grape", "carrot"}, Weight: 540, Gender: "f"},
+		&Unicorn{Name: "Dunx", Dob: time.Date(1976, 6, 18, 18, 180, 0, 0, time.Local), Loves: []string{"grape", "watermelon"}, Weight: 704, Gender: "m", Vampires: 165})
+	if err != nil {
+		return nil, err
+	}
+	return c, nil
+}
+
+func cleanupUnicorns(c *mgo.Collection) error {
+	_, err := c.RemoveAll(struct{}{})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// ch01
+	if err = ch01(session); err != nil {
+		panic(err)
+	}
+}
+
+func ch01(s *mgo.Session) error {
+	fmt.Println("Ch01")
+
+	c, err := insertUnicorns(s)
+	if err != nil {
+		return err
+	}
+
+	//
+	fmt.Println("性別が男で体重が700ポンドより大きいユニコーンを探す")
+	var unicorns []Unicorn
+	err = c.Find(
+		bson.M{"gender": "m",
+			"weight": bson.M{"$gt": 700}}).All(&unicorns)
+	if err != nil {
+		return err
+	}
+	for _, u := range unicorns {
+		fmt.Printf("%v\n", u)
+	}
+
+	// TODO: $exists ...type 定義が必要. Name: "Nimue" は Vampires なし
+
+	//
+	fmt.Println("全ての女性のユニコーンの中から、りんごかオレンジが好き、もしくは体重が 500ポンド未満の条件で検索します")
+	err = c.Find(
+		bson.M{"gender": "f",
+			"$or": []bson.M{
+				bson.M{"loves": "apple"},
+				bson.M{"loves": "orange"},
+				bson.M{"weight": bson.M{"$lt": 500}}}}).All(&unicorns)
+	if err != nil {
+		return err
+	}
+	for _, u := range unicorns {
+		fmt.Printf("%v\n", u)
+	}
+	// Cleanup
+	if err = cleanupUnicorns(c); err != nil {
+		panic(err)
+	}
+	return nil
+}
+
